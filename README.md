372b48479e0f15a522d51b6cf72ee6bae039691a
Vicenzo Naves <vicenzo@gmail.com>
2020-07-28 13:59:41 -0700

    Material UI Demo

--- README.md
+++ README.md
@@ -1,44 +1,27 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+Trying out new libraries to evaluate ease of use.
 
-In the project directory, you can run:
+Tried 3 different libraries:
 
-### `yarn start`
+[Material-UI](https://material-ui.com/)
+[Grommet](https://grommet.io)
+[PrimeReact](https://primefaces.org/primereact)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Current "active" library: Material-UI 
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Run `npm install` on master to ensure all the used libraries are installed
 
-### `yarn test`
+To switch between the POCs (for cleaner code and laziness of not making multiple pages and a dropdown selector) switch
+ between the commits. 
+ Material UI - HEAD
+ Grommet - 9dcebc90
+ PrimeReact - da4af8bd
+ 
+### What was done to evaluate the libraries.
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+* Create a login form
+* Create a nav Header with 2 layers, one for Logo + Logout, one for Application Name + NavBar
+  * NavBar should have a highlight effect and 'active' state
+* Login form submit would NOT do anything but open a modal with some content inside.
+* Apply color scheme and customizations to the default libraries
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }  from 'react'
+import './App.scss'
+import logo from './genericLogo.svg'
+import {
+    AppBar,
+    Box,
+    Button, Dialog,
+    FormControl, Grid, IconButton,
+    TextField,
+    Toolbar,
+    Typography,
+    withStyles
+} from "@material-ui/core"
+import {ToggleButtonGroup, ToggleButton} from "@material-ui/lab"
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import {Visibility, VisibilityOff} from "@material-ui/icons";
 
-function App() {
+const NavButton = withStyles({
+    root: {
+        borderRadius: 0,
+        border: "unset",
+        borderBottom: "solid 3px transparent",
+        color: "white",
+        maxHeight: "3.6rem",
+        alignItems: "unset",
+        '&:hover': {
+            borderBottom: "coral solid 3px",
+            backgroundColor: "unset"
+        },
+    },
+    selected: {
+        borderBottom: "coral solid 3px",
+        color: "white !important"
+    }
+})(ToggleButton)
+
+const App = () => {
+    const [username, setUsername] = useState('')
+    const [ password, setPassword] = useState('')
+    const [modalVisibility, setModalVisibility] = useState(false)
+    const [activeNav, setActiveNav] = useState()
+    const [showPassword, setShowPassword] = useState(false)
+    const handleActiveButton = (event: any, newActive: any) => {
+        setActiveNav(newActive)
+    }
+
+    const handleClickShowPassword = () => {
+       setShowPassword(!showPassword)
+    }
+
+    const handleMouseDownPassword = (event: any) => {
+        event.preventDefault();
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+          <div className='flex-grow'>
+          <AppBar position='static' className="header-bar">
+              <Toolbar variant="dense" className='top-logo-bar'>
+                  <Box className='flex-grow'>
+                    <img src={logo} className='logo' alt="logo"/>
+                  </Box>
+                 <Button>
+                     Logout  <ExitToAppIcon/>
+                 </Button>
+              </Toolbar>
+              <Toolbar disableGutters variant='dense' className='nav-bar'>
+                  <Typography variant="h6" className='app-name'>
+                      Extra Long Apex App Name
+                  </Typography>
+                  <ToggleButtonGroup exclusive size='large' value={activeNav} onChange={handleActiveButton}>
+                    <NavButton value="nav-1" disableFocusRipple className="NavButton">Nav 1</NavButton>
+                    <NavButton value="nav-2" disableFocusRipple className="NavButton">Nav 2</NavButton>
+                    <NavButton value="nav-3" disableFocusRipple className="NavButton">Nav 3</NavButton>
+                    <NavButton value="nav-4" disableFocusRipple className="NavButton">Nav 4</NavButton>
+                  </ToggleButtonGroup>
+              </Toolbar>
+          </AppBar>
+          </div>
+          <Grid container spacing={3}>
+              <Grid item xs={12}/>
+              <Grid item xs={12}/>
+              <Grid item xs={4}/>
+              <Grid item xs={3}>
+                  <FormControl fullWidth>
+                      <TextField
+                          type='text'
+                          value={username}
+                          onChange={(e) => setUsername(e.currentTarget.value)}
+                          size="medium"
+                          label="Username"
+                      />
+                      <TextField label="Password"
+                                         type={showPassword ? 'text' : 'password'}
+                                         value={password}
+                                         onChange={(e) => setPassword(e.currentTarget.value)}
+                                         size="medium"
+                                 InputProps={{
+                                     endAdornment: (
+                                         <IconButton
+                                             aria-label="toggle password visibility"
+                                             onClick={handleClickShowPassword}
+                                             onMouseDown={handleMouseDownPassword}
+                                         >
+                                             {showPassword ? <Visibility /> : <VisibilityOff />}
+                                         </IconButton>
+                                     )
+                                 }}
+                      />
+                      <Button variant="contained" color="primary" className="SubmitButton"
+                              onClick={() => setModalVisibility(true)}>Login</Button>
+                  </FormControl>
+              </Grid>
+          </Grid>
+          <Dialog onClose={()=>setModalVisibility(false)} open={modalVisibility}>
+              <Typography>You've entered a {username} and a password with length equal to {password.length}</Typography>
+              <Button variant="contained" color="primary" onClick={() => setModalVisibility(false)}>Close</Button>
+          </Dialog>
+          </>
+  )
 }
 
-export default App;
+export default App
